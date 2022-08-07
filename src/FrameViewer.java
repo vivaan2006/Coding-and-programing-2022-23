4e2f47f936f0001f5f85b5f7f11746b02a5185df
AdhuBavu2 <89229725+AdhuBavu2@users.noreply.github.com>
2022-08-07 16:46:40 -0600

    Delete FrameViewer.java

--- src/FrameViewer.java
+++
@@ -1,7 +0,0 @@
-public class FrameViewer {
-    public static void main(String[] args) {
-        IDandPasswordsStudent idsStudent = new IDandPasswordsStudent();
-        IDandPasswordsAdmin idsAdmin = new IDandPasswordsAdmin();
-        EntryPage entryPage = new EntryPage();
-    }
-}
